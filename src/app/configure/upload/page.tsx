6dd20c52108e6124c932ed0dcd4d138b83550a72
Vladyslav Zaikovskyi <vladyslav.zaikovskyi@gmail.com>
2024-09-11 19:59:39 +0300

    preview desig page + db connected

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -21,7 +21,7 @@ const Page = () => {
       const configId = data.serverData.configId;
 
       startTransition(() => {
-        router.push(`/configure/design?if=${configId}`);
+        router.push(`/configure/design?id=${configId}`);
       });
     },
 
@@ -51,7 +51,7 @@ const Page = () => {
   return (
     <div
       className={cn(
-        'relative h-full flex-1 my-16 w-full rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center',
+        'relative h-full flex-1 my-16 w-full grainy-light rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center',
         { 'ring-blue-900/25 bg-blue-900/10': isDragOver }
       )}>
       <div className="relative flex flex-1 flex-col items-center justify-center w-full">
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
 
-        <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
+        <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
           <div className="flex-1 flex flex-col h-full">{children}</div>
           <Footer />
         </main>
