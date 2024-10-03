8ff970403e47c7e262278c7ac9cd2d492d089f44
Vladyslav Zaikovskyi <vladyslav.zaikovskyi@gmail.com>
2024-10-03 22:43:15 +0300

    final deploy

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -44,7 +44,7 @@ const DesignConfigurator = ({
   const { toast } = useToast();
   const router = useRouter();
 
-  const { mutate: saveConfig } = useMutation({
+  const { mutate: saveConfig, isPending } = useMutation({
     mutationKey: ['save-config'],
     mutationFn: async (args: saveConfigArgs) => {
       await Promise.all([saveConfiguration(), _saveConfig(args)]);
@@ -153,28 +153,28 @@ const DesignConfigurator = ({
   }
 
   return (
-    <div className="relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20">
+    <div className='relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20'>
       <div
         ref={containerRef}
-        className="relative h-[37.5rem] overflow-hidden col-span-2 w-full
+        className='relative h-[37.5rem] overflow-hidden col-span-2 w-full
         max-w-4xl flex items-center justify-center rounded-lg border-2
         border-dashed border-gray-300 p-12 text-center focus:outline-none
-        focus:ring-2 focus:ring-primary focus:ring-offset-2">
-        <div className="relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]">
+        focus:ring-2 focus:ring-primary focus:ring-offset-2'>
+        <div className='relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]'>
           <AspectRatio
             ref={phoneCaseRef}
             ratio={896 / 1831}
-            className="pointer-events-none relative z-50 aspect-[896/1831] w-full">
+            className='pointer-events-none relative z-50 aspect-[896/1831] w-full'>
             <NextImage
               fill
-              alt="phone image"
-              src="/phone-template.png"
-              className="pointer-events-none z-50 select-none"
+              alt='phone image'
+              src='/phone-template.png'
+              className='pointer-events-none z-50 select-none'
             />
           </AspectRatio>
           <div
-            className="absolute z-40 inset-0 left-[3px] top-px right-[3px]
-            bottom-px rounded-[32px] shadow-[0_0_0_99999px_rgba(229,231,235,0.6)]"
+            className='absolute z-40 inset-0 left-[3px] top-px right-[3px]
+            bottom-px rounded-[32px] shadow-[0_0_0_99999px_rgba(229,231,235,0.6)]'
           />
 
           <div
@@ -203,7 +203,7 @@ const DesignConfigurator = ({
             const { x, y } = data;
             setRenderedPosition({ x, y });
           }}
-          className="absolute z-20 border-[3px] border-primary"
+          className='absolute z-20 border-[3px] border-primary'
           lockAspectRatio
           resizeHandleComponent={{
             bottomRight: <HandleComponent />,
@@ -211,12 +211,12 @@ const DesignConfigurator = ({
             topRight: <HandleComponent />,
             topLeft: <HandleComponent />,
           }}>
-          <div className="relative w-full h-full">
+          <div className='relative w-full h-full'>
             <NextImage
               src={imageUrl}
               fill
-              alt="your image"
-              className="pointer-events-none"
+              alt='your image'
+              className='pointer-events-none'
             />
           </div>
         </Rnd>
@@ -224,21 +224,21 @@ const DesignConfigurator = ({
 
       {/* Configuration options */}
 
-      <div className="h-[37.5rem] w-full col-span-full lg:col-span-1 flex flex-col bg-white">
-        <ScrollArea className="relative flex-1 overflow-auto">
+      <div className='h-[37.5rem] w-full col-span-full lg:col-span-1 flex flex-col bg-white'>
+        <ScrollArea className='relative flex-1 overflow-auto'>
           <div
             aria-hidden
-            className="absolute z-10 inset-x-0 bottom-0
-            h-12 bg-gradient-to-t from-white pointer-events-auto"
+            className='absolute z-10 inset-x-0 bottom-0
+            h-12 bg-gradient-to-t from-white pointer-events-auto'
           />
-          <div className="px-8 pb-12 pt-8">
-            <h2 className="tracking-tight font-bold text-3xl">
+          <div className='px-8 pb-12 pt-8'>
+            <h2 className='tracking-tight font-bold text-3xl'>
               Customize your case
             </h2>
-            <div className="w-full h-px bg-zinc-200 my-6" />
+            <div className='w-full h-px bg-zinc-200 my-6' />
 
-            <div className="relative mt-4 h-full flex flex-col justify-between">
-              <div className="flex flex-col gap-6">
+            <div className='relative mt-4 h-full flex flex-col justify-between'>
+              <div className='flex flex-col gap-6'>
                 <RadioGroup
                   value={options.color}
                   onChange={(value) => {
@@ -248,7 +248,7 @@ const DesignConfigurator = ({
                     }));
                   }}>
                   <Label>Color: {options.color.label}</Label>
-                  <div className="mt-3 flex items-center space-x-3">
+                  <div className='mt-3 flex items-center space-x-3'>
                     {COLORS.map((color) => (
                       <Radio
                         key={color.label}
@@ -269,16 +269,16 @@ const DesignConfigurator = ({
                   </div>
                 </RadioGroup>
 
-                <div className="relative flex flex-col gap-3 w-full">
+                <div className='relative flex flex-col gap-3 w-full'>
                   <Label>Model</Label>
                   <DropdownMenu>
                     <DropdownMenuTrigger asChild>
                       <Button
-                        variant="outline"
-                        role="combobox"
-                        className="w-full justify-between">
+                        variant='outline'
+                        role='combobox'
+                        className='w-full justify-between'>
                         {options.model.label}
-                        <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                        <ChevronsUpDown className='ml-2 h-4 w-4 shrink-0 opacity-50' />
                       </Button>
                     </DropdownMenuTrigger>
                     <DropdownMenuContent>
@@ -324,7 +324,7 @@ const DesignConfigurator = ({
                       <Label>
                         {name.slice(0, 1).toUpperCase() + name.slice(1)}
                       </Label>
-                      <div className="mt-3 space-y-4">
+                      <div className='mt-3 space-y-4'>
                         {selectableOptions.map((option) => (
                           <Radio
                             key={option.value}
@@ -335,16 +335,16 @@ const DesignConfigurator = ({
                                 { 'border-primary': checked }
                               )
                             }>
-                            <span className="flex items-center">
-                              <span className="flex flex-col text-sm">
-                                <Label className="font-medium text-gray-900">
+                            <span className='flex items-center'>
+                              <span className='flex flex-col text-sm'>
+                                <Label className='font-medium text-gray-900'>
                                   {option.label}
                                 </Label>
                                 {option.description ? (
                                   <Description
-                                    as="span"
-                                    className="text-gray-500">
-                                    <span className="block sm:inline">
+                                    as='span'
+                                    className='text-gray-500'>
+                                    <span className='block sm:inline'>
                                       {option.description}
                                     </span>
                                   </Description>
@@ -353,9 +353,9 @@ const DesignConfigurator = ({
                             </span>
 
                             <Description
-                              as="span"
-                              className="mt-2 flex text-sm sm:ml-4 sm:mt-0 sm:flex-col sm:text-right">
-                              <span className="font-medium text-gray-900">
+                              as='span'
+                              className='mt-2 flex text-sm sm:ml-4 sm:mt-0 sm:flex-col sm:text-right'>
+                              <span className='font-medium text-gray-900'>
                                 {formatPrice(option.price / 100)}
                               </span>
                             </Description>
@@ -370,17 +370,20 @@ const DesignConfigurator = ({
           </div>
         </ScrollArea>
 
-        <div className="w-full px-8 h-16 bg-white">
-          <div className="h-px w-full bg-zinc-200" />
-          <div className="w-full h-full flex justify-center items-center">
-            <div className=" w-full flex gap-6 items-center">
-              <p className="font-medium whitespace-nowrap">
+        <div className='w-full px-8 h-16 bg-white'>
+          <div className='h-px w-full bg-zinc-200' />
+          <div className='w-full h-full flex justify-center items-center'>
+            <div className=' w-full flex gap-6 items-center'>
+              <p className='font-medium whitespace-nowrap'>
                 {formatPrice(
                   (BASE_PRICE + options.finish.price + options.material.price) /
                     100
                 )}
               </p>
               <Button
+                isLoading={isPending}
+                disabled={isPending}
+                loadingText='Saving'
                 onClick={() =>
                   saveConfig({
                     configId,
@@ -390,10 +393,10 @@ const DesignConfigurator = ({
                     model: options.model.value,
                   })
                 }
-                size="sm"
-                className="w-full">
+                size='sm'
+                className='w-full'>
                 Continue
-                <ArrowRight className="h-4 ml-1.5 w-4 inline" />
+                <ArrowRight className='h-4 ml-1.5 w-4 inline' />
               </Button>
             </div>
           </div>
